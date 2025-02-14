32cf2c3429cd7b75ffa4d4ab7f12d2e5a671b4eb
Martin Haynes <martin@dotmh.io>
2025-02-14 13:32:04 +0000

    fix: toc links

--- scripts/hbs-helpers.ts
+++ scripts/hbs-helpers.ts
@@ -1,7 +1,15 @@
 import hb from "handlebars";
 
-const asAnchor = (name: string): hb.SafeString => {
-    return new hb.SafeString(name.toLowerCase().replace(/\s/g, "-"));
+const asAnchor = (emoji: string, name: string): hb.SafeString => {
+    return new hb.SafeString(
+        '#' + `${emoji} ${name}`
+            .toLowerCase()
+            .split('')
+            .map((char, idx) => {
+                return char.match(/[a-zA-Z0-9_-]/) ? char : (!!idx ? '-' : '');
+            })
+            .join('')
+    );
 };
 
 const asEmoji = (emojiName: string): hb.SafeString => {
