82db49efab06d9d234e1fc0fb75fd1542228834f
Martin Haynes <martin@dotmh.io>
2025-02-19 17:15:17 +0000

    fix(build): strip trailing hyphens on toc links

--- scripts/hbs-helpers.ts
+++ scripts/hbs-helpers.ts
@@ -1,15 +1,21 @@
 import hb from "handlebars";
 
 const asAnchor = (emoji: string, name: string): hb.SafeString => {
-    return new hb.SafeString(
-        '#' + `${emoji} ${name}`
-            .toLowerCase()
+    const fullString = `${emoji} ${name}`;
+    const isFirst = (idx: number) => idx === 0;
+    const isLast = (idx: number) => idx === fullString.length - 1;
+    const isFirstOrLast = (idx: number) => isFirst(idx) || isLast(idx);
+
+    const proccessed = (
+        fullString.toLowerCase()
             .split('')
             .map((char, idx) => {
-                return char.match(/[a-zA-Z0-9_-]/) ? char : (!!idx ? '-' : '');
+                return char.match(/[a-zA-Z0-9_-]/) ? char : (!isFirstOrLast(idx) ? '-' : '');
             })
             .join('')
     );
+
+    return new hb.SafeString(`#${proccessed}`);
 };
 
 const asEmoji = (emojiName: string): hb.SafeString => {
